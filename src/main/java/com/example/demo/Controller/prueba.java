d5e4e47f2b0ba5ac2f8ce2ff884a05377180f666
luisdanilogomezperez <luisdanilogp@ufps.edu.co>
2022-04-05 13:49:43 -0500

    este es mi primer comit

---
+++ src/main/java/com/example/demo/Controller/prueba.java
@@ -0,0 +1,16 @@
+package com.example.demo.Controller;
+
+public class prueba {
+    
+    private String pruiebaGit;
+
+    public String getPruiebaGit() {
+        return pruiebaGit;
+    }
+
+    public void setPruiebaGit(String pruiebaGit) {
+        this.pruiebaGit = pruiebaGit;
+    }
+
+    
+}
